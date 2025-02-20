283e2e02723f1858a0d5cdc0d839e558719bef6f
lAquilaP <161894977+lAquilaP@users.noreply.github.com>
2025-02-19 21:26:48 -0300

    Add files via upload

---
+++ helloWord.java
@@ -0,0 +1,13 @@
+
+public class helloWord {
+
+	public String msg;
+	
+	public helloWord() {		
+		this.msg = "Hello word!";
+	}
+	
+	public void show(){
+		System.out.println(this.msg);
+	}
+}
---
+++ inicial.java
@@ -0,0 +1,9 @@
+
+public class inicial {
+
+	public static void main(String[] args) {
+		helloWord hword = new helloWord();
+		hword.show();
+	}
+
+}
