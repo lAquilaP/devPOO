a30ffb7af3c9560f87fbfb2c83c1fd469c7ebf08
lAquilaP <161894977+lAquilaP@users.noreply.github.com>
2025-03-10 11:30:52 -0300

    updates

--- telas/Calculadora.java
+++ telas/Calculadora.java
@@ -1,7 +1,9 @@
 package telas;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;	
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JFrame;
@@ -12,7 +14,6 @@ public class Calculadora {
 	
 	private JFrame janela = new JFrame();
 	private JTextField display = new JTextField();
-	//private Font fonte = new Font("Arial" Font.Bold, 20);
 	
 	private JButton btn7 = new JButton("7");
 	private JButton btn8 = new JButton("8");
@@ -32,6 +33,14 @@ public class Calculadora {
 	private JButton btn3 = new JButton("3");
 	private JButton btnMulti = new JButton("*");
 	
+	private JButton btn0 = new JButton("0");
+	private JButton btnPonto = new JButton(".");
+	private JButton btnTrocaValor = new JButton("+/-");
+	private JButton btnDivisao = new JButton("/");
+	
+	private JButton btnIgual = new JButton("=");
+
+	
 
 
 	
@@ -39,10 +48,21 @@ public class Calculadora {
 		
 		this.janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.janela.setTitle("Calculadora");
-		this.janela.setBounds(500, 500, 215, 215);
+		this.janela.setBounds(500, 500, 240, 375);
 		this.janela.setLayout(null);
 		
 		this.display.setBounds(5, 5, 215, 40);
+		this.display.addKeyListener(new KeyAdapter() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char c = e.getKeyChar();
+				if(!(Character.isDigit(c) || c == '+' || c == '-' || c == '/' || c == '=')) {
+					e.consume();
+				}
+				
+			}
+		});
 		this.btn7.setBounds(5, 55, 50, 50);
 		this.btn7.addActionListener(new ActionListener() {
 			
@@ -52,8 +72,146 @@ public class Calculadora {
 			}
 		});
 		this.btn8.setBounds(60, 55, 50, 50);
+		this.btn8.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "8");
+			}
+		});
 		this.btn9.setBounds(115, 55, 50, 50);
+		this.btn9.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "9");
+			}
+		});
+		
 		this.btnMais.setBounds(170, 55, 50, 50);
+		this.btnMais.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String texto = display.getText();
+				if(!texto.isEmpty() && !texto.endsWith("+") && !texto.endsWith("-") && !texto.endsWith("/")) {
+					display.setText(display.getText() + "+");
+				};
+				
+				
+			}
+		});
+		
+		this.btn4.setBounds(5, 110, 50, 50);
+		this.btn4.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "4");
+			}
+		});
+		this.btn5.setBounds(60, 110, 50, 50);
+		this.btn5.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "5");
+			}
+		});
+		this.btn6.setBounds(115, 110, 50, 50);
+		this.btn6.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "6");
+			}
+		});
+		
+		this.btnMenos.setBounds(170, 110, 50, 50);
+		this.btnMenos.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "-");
+			}
+		});
+		
+		this.btn1.setBounds(5, 165, 50, 50);
+		this.btn1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "1");
+			}
+		});
+		this.btn2.setBounds(60, 165, 50, 50);
+		this.btn2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "2");
+			}
+		});
+		this.btn3.setBounds(115, 165, 50, 50);
+		this.btn3.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "3");
+			}
+		});
+		
+		this.btnMulti.setBounds(170, 165, 50, 50);
+		this.btnMulti.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "*");
+			}
+		});
+		
+		
+		this.btn0.setBounds(5, 220, 50, 50);
+		this.btn0.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "0");
+			}
+		});
+		this.btnPonto.setBounds(60, 220, 50, 50);
+		this.btnPonto.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + ".");
+			}
+		});
+		this.btnTrocaValor.setBounds(115, 220, 50, 50);
+		this.btnTrocaValor.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "false");
+			}
+		});
+		
+		this.btnDivisao.setBounds(170, 220, 50, 50);
+		this.btnDivisao.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "/");
+			}
+		});
+		this.btnIgual.setBounds(5, 275, 215, 50);
+		this.btnIgual.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				display.setText(display.getText() + "true");
+			}
+		});
 		
 		this.janela.add(display);
 		this.janela.add(btn7);
@@ -61,9 +219,26 @@ public class Calculadora {
 		this.janela.add(btn9);
 		this.janela.add(btnMais);
 
+		this.janela.add(btn4);
+		this.janela.add(btn5);
+		this.janela.add(btn6);
+		this.janela.add(btnMenos);
 		
+		this.janela.add(btn1);
+		this.janela.add(btn2);
+		this.janela.add(btn3);
+		this.janela.add(btnMulti);
 		
+		this.janela.add(btn0);
+		this.janela.add(btnPonto);
+		this.janela.add(btnTrocaValor);
+		this.janela.add(btnDivisao);
+		
+		this.janela.add(btnIgual);
+
 		this.janela.setVisible(true);
+		
+		
 		
 	}
 }
